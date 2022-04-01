e8bea9f897ac6de5c24cc2f59499cd8caf583840
remyinthecloud <102815890+remyinthecloud@users.noreply.github.com>
2022-04-01 11:09:15 -0400

    Add files via upload

---
+++ juicebox_band_generator.py
@@ -0,0 +1,19 @@
+#Juicebox being nervous as usual
+print("I'm nervous, uh.. I mean Juicebox!")
+
+#Ask user to enter their name
+user_name = input("What is your name?\n")
+
+#Juicebox is very sweet
+print("Okay " + user_name)
+
+#Ask user where they grew up
+city = input("What city did you grow up in?\n")
+
+#This is a tough one. Ask for their favorite pet name. This could take a while Juicebox. Hang tight
+pet_name = input("What was your favorite pet's name growing up?\n")
+
+#Generate users band name! Now they can go rock the world!!
+print("Your band name would be " + city + " " + pet_name + "!")
+
+print("\nNow go rock the world and leave me be. Juicebox is thirsty!")
